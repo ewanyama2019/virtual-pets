261fab0c8855a303c4da3a5b4bfd2a9eadac156f
ewanyama2019 <wafulawanyama@gmail.com>
2019-05-29 22:02:44 +0300

    Added Constructor Person and getter methods for getName and getEmail. also added test for get name and email

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,17 +1,15 @@
 public class Person {
-
-    public Person () {
-        private String name;
-        private String email;
-
-        //Constructor for Person Class Properties
-        public  Person(String name, String email) {
-            this.name = name;
-            this.email = email;
-        }
+    private String name;
+    private String email;
 
 
-        //getter Methods for Name and email - class Person
+    //Constructor for Person Class Properties//
+    public Person (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    //getter Methods for Name and email - class Person
 
         public String getName() {
             return name;
@@ -20,6 +18,18 @@ public class Person {
         public String getEmail() {
             return email;
         }
+//
+//        //Override equals
+//        @Override
+//                public boolean equals(Object otherPerson) {
+//            if (!(otherPerson instanceof Person)) {
+//                return false;
+//            } else {
+//                person newPerson = (Person) otherPerson;
+//                return this.getName().equals(newPerson.getName()) && this.getEmail().equals(newPerson.getEmail());
+//            }
+//        }
+
 
     }
-}
+
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -4,20 +4,29 @@ import static org.junit.Assert.*;
 public class PersonTest {
 
     @Test
-    public void person_InstantiateCorrectly_true() {
+    public void person_InstantiatesCorrectly_true() {
         Person testPerson = new Person("Henry", "[email protected]");
         assertEquals(true, testPerson instanceof Person);
     }
 
     @Test
-    public void getName_personInstatiatesWithName_Henry () {
-        Person testPerson = new Person ("Henry", "[email protected]");
+    public void getName_personInstantiatesWithName_Henry() {
+        Person testPerson = new Person("Henry", "[email protected]");
         assertEquals("Henry", testPerson.getName());
     }
 
     @Test
-    public void getEmail_personInstantiatesWithEmail_String () {
-        Person testPerson = new Person ("Henry", "[email protected]");
+    public void getEmail_personInstantiatesWithEmail_String() {
+        Person testPerson = new Person("Henry", "[email protected]");
         assertEquals("[email protected]", testPerson.getEmail());
     }
+//
+//    @Test
+//    public void equals_returnsTrueIfNameAndEmailAreSame_true (){
+//        Person firstPerson = new Person("Henry", "[email protected]");
+//        Person anotherPerson = new Person("Henry", "[email protected]");
+//        assertTrue(firstPerson.equals(anotherPerson));
+//    }
+
+
 }
